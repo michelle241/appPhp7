db99f909d857d26a20a01092ba1ac950af2f4a83
michelle241 <58831907+michelle241@users.noreply.github.com>
2019-12-13 13:02:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,71 +2,63 @@
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
-    <title>IntroPhp7 Grupo XA</title>
-    <link rel="stylesheet" href="css/estilos.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Science Technology Engineerig & Mathematics</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/menu.css"/>
     <link rel="stylesheet" href="css/problema.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <script>
+        function solucion(){
+        var masa=6578;
+        var gravedad=9.8;
+        var densidad=masa/gravedad;
+        
+        var d=document.getElementById("resultado");
+        d.innerHTML='La densidad es: '+densidad+' kg/m3';
+        }
+    </script>
 </head>
 <body>
-   <section class="wrapper">
-    <header>
-      <h1 class="logo"><a href="index.php">IntroPHP7</a></h1>
-      <nav>
-      <ul>
-        <li><a href="index.php" class="current">Inicio</a></li>
-        <li><a href="#">Otros Ejemplos Php7</a></li>
-      </ul>
-      </nav>
+    <section id="contenedor">
+    <header id="gridHeader">
+    <h1>Science Technology Engineerig & Mathematics</h1>
+    <h2>Resolver problemas de Ciencia e Ingeniería con JS</h2>
+    <h2>Nombre: Michelle Roque López  No. de control: 17091152</h2>
+    <img src="fotoSelfie.jpg" alt="su foto">
     </header>
-<section id="contenedor">
-<section  class="problema">
-  <h2>Problema: Cálcular la densidad de la Tierra</h2>
-  <p>Descripción:</p>
-  <p>El radio de la Tierra es 6 378 km y su masa es 5.97E24 kg. <br>
-a) ¿Cuál es la densidad promedio de la Tierra?<br>
-b) Si el radio de la Tierra se redujera a una tercera parte y su masa no cambiará, cuál serı́a su densidad?<br>
-c) Si el radio de la Tierra y su masa se redujera a una
-tercera parte, ¿su densidad serı́a la misma? ¿por qué?</p>
-</section>
-<section class="formulas">
-<h2>Fórmulas</h2>
-    Densidad = Masa/Volumen<br>
-    Volumen=(4/3)pi*r*r*r
-</section>
-<section class="datos">
-<h2>Datos:</h2>
-   m= 5.97E24 Kg. <br>
-   r= 6378 Km.
-</section>
-<section class="calculos">
-<h2>Solución</h2>
-   <p>a)La densidad de la Tierra es:<br>
-    densidad = 5.97E24 Kg/volumen (m) <br>
-    volumen= (4/3)*pi*(6378000)*(6378000)*(6378000)</p>
-</section>
-<?php
-     function calcula_densidad(){
-        $PI=3.141592;
-        $masa= 5.97E24;
-        $radio= 6378e3;
-        $volumen= (4/3)*$PI*$radio*$radio*$radio;
-        $densidad= $masa/$volumen;
-        return $densidad;
-     }
-?>
-<section class="resultado">
-<h2>Resultado:</h2>
-<div id="resultadoA"></div>
-<?php
- print "<h1> resultado: densidad = ".calcula_densidad(). " kg/(metro cubico)</h1>";
-?>
-</section>
-</section>
-    <footer class="pie">
-     Creative Commons versión 3.0 SciSoft 2019
+    </section>
+    <section id="problema"  class="gridProblema">
+      <section id=descripcion>
+            <h2>Problema: Cálcular la densidad</h2>
+            <p>Descripción:</p>
+            <p>¿Cual es la densidad de un aceite cuyo peso especifico es de 6578 N/m3? <br>
+      </section>
+      <section id="datos">
+            <h2>Datos:</h2>
+            masa = m = 6578 N/m**3 <br>
+            densidad = d = 6578/9.8 m/s2 <br>
+            aceleracion de gravedad = g = 980665 m/s2
+            
+      </section>
+      <section id="formulas">
+            <h2>Fórmulas</h2>
+            d=masa/volumen
+      </section>
+      <section id="solucion">
+            <h2>Solución</h2>
+            <p>La densidad es:<br>
+             d=671.2 kg/m3</p>
+             <button onclick="solucion()">Presiona para calcular</button>
+      </section>
+      
+      <section id="resultado"></section>
+    </section>
+    <footer id="gridPie">
+        <p>Solution Science Problems. 2019 Creative Commons 3.0</p>
     </footer>
-   </section>
 </body>
 </html>
